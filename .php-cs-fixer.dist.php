c61fd20d3357ed82cb3fefe1e48a4a2cf6c58eb6
Oskar Barcz <kontakt@archi-tektur.pl>
2021-06-15 15:21:46 +0200

    Add code style fixer to environment

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,15 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('vendor');
+
+return (new PhpCsFixer\Config())
+    ->setRules(
+        [
+            '@PSR12'   => true,
+            '@Symfony' => true,
+        ]
+    )
+    ->setFinder($finder);
