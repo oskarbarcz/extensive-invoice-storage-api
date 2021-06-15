6cc90c93ed86c579ee66799f44ba61fad07eea1b
Oskar Barcz <kontakt@archi-tektur.pl>
2021-06-15 22:31:40 +0200

    Add proper Database mapping

--- migrations/Version20210615202640.php
+++ migrations/Version20210615202640.php
@@ -14,14 +14,23 @@ final class Version20210615202640 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create initial invoices table';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(
-            'CREATE TABLE invoices (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', name VARCHAR(255) NOT NULL, file_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+            'CREATE TABLE invoices (
+                     id      BINARY(16)   NOT NULL COMMENT \'(DC2Type:uuid)\',
+                     name    VARCHAR(255) NOT NULL,
+                     file_id BINARY(16)   NOT NULL COMMENT \'(DC2Type:uuid)\',
+                     type    VARCHAR(255) NOT NULL,
+                     PRIMARY KEY(id)
+                )
+                DEFAULT CHARACTER SET utf8mb4 
+                COLLATE `utf8mb4_unicode_ci` 
+                ENGINE = InnoDB'
         );
     }
 
