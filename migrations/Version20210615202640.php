540eaaf0d993cd941ff7711b8676b895eb3f66a5
Oskar Barcz <kontakt@archi-tektur.pl>
2021-06-15 22:38:40 +0200

    Fix migration style

--- migrations/Version20210615202640.php
+++ migrations/Version20210615202640.php
@@ -16,9 +16,7 @@ final class Version20210615202640 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql(
-            'CREATE TABLE invoices (
+        $sql = 'CREATE TABLE invoices (
                      id      BINARY(16)   NOT NULL COMMENT \'(DC2Type:uuid)\',
                      name    VARCHAR(255) NOT NULL,
                      file_id BINARY(16)   NOT NULL COMMENT \'(DC2Type:uuid)\',
@@ -27,13 +25,14 @@ final class Version20210615202640 extends AbstractMigration
                 )
                 DEFAULT CHARACTER SET utf8mb4 
                 COLLATE `utf8mb4_unicode_ci` 
-                ENGINE = InnoDB'
-        );
+                ENGINE = InnoDB';
+
+        $this->addSql($sql);
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE invoices');
+        $sql = 'DROP TABLE invoices';
+        $this->addSql($sql);
     }
 }
