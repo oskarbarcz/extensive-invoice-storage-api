a94b66de337ab38ff40e44b126178179f3e2b650
Oskar Barcz <kontakt@archi-tektur.pl>
2021-07-09 13:03:32 +0200

    Add proper response code (422) for failing validation

--- src_architool/Response/OpenApiResponse.php
+++ src_architool/Response/OpenApiResponse.php
@@ -12,7 +12,7 @@ class OpenApiResponse extends JsonResponse
 
     private function __construct(
         mixed $data = null,
-        string | null $message = null,
+        string|null $message = null,
         int $status = self::HTTP_OK,
         array $headers = []
     ) {
@@ -26,7 +26,7 @@ class OpenApiResponse extends JsonResponse
         parent::__construct($content, $status, $headers, false);
     }
 
-    public static function empty(string | null $message = null, int $status = self::HTTP_NO_CONTENT): self
+    public static function empty(string|null $message = null, int $status = self::HTTP_NO_CONTENT): self
     {
         if (null === $message) {
             $message = self::$statusTexts[$status];
@@ -35,7 +35,7 @@ class OpenApiResponse extends JsonResponse
         return new self(null, $message, $status);
     }
 
-    public static function ok(string | null $message = null, array | null $data = null): self
+    public static function ok(string|null $message = null, array|null $data = null): self
     {
         return new self($data, $message, self::HTTP_OK);
     }
@@ -55,14 +55,14 @@ class OpenApiResponse extends JsonResponse
         return new self($item, null, $status);
     }
 
-    public static function notFound(string | null $message): self
+    public static function notFound(string|null $message): self
     {
         return new self(null, $message, self::HTTP_NOT_FOUND);
     }
 
     public static function validationFail(array $constraintViolations): self
     {
-        return new self($constraintViolations, 'validation.fail', self::HTTP_BAD_REQUEST);
+        return new self($constraintViolations, 'validation.fail', self::HTTP_UNPROCESSABLE_ENTITY);
     }
 
     public static function exception(string $message = null, int $code = self::HTTP_INTERNAL_SERVER_ERROR): self
