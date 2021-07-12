de87d9825e21166f8739b081fe037fe1135b02e8
Oskar Barcz <kontakt@archi-tektur.pl>
2021-07-12 11:22:09 +0200

    Add invoice unit test

--- tests/Domain/InvoiceTest.php
+++ tests/Domain/InvoiceTest.php
@@ -4,7 +4,33 @@ declare(strict_types=1);
 
 namespace App\Tests\Domain;
 
-class InvoiceTest
-{
+use App\Domain\Exception\DomainLogicException;
+use App\Domain\Invoice;
+use App\Domain\User;
+use App\Domain\ValueObject\InvoiceType;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Uid\Uuid;
 
+class InvoiceTest extends TestCase
+{
+    public function testInvoiceFileShouldNotAllowOverwrite(): void
+    {
+        $testUser = $this->createStub(User::class);
+
+        $invoice = self::createTestInvoice($testUser);
+        $invoice->setFile('test_file.pdf');
+
+        // try to override
+        self::expectException(DomainLogicException::class);
+        $invoice->setFile('another_file.pdf');
+    }
+
+    public static function createTestInvoice(User $user): Invoice
+    {
+        $id = Uuid::v4();
+        $name = 'Test Invoice';
+        $type = new InvoiceType('cost');
+
+        return new Invoice($id, $name, $type->toString(), $user);
+    }
 }
