bdf1e8d882ea1db4cb2b0ff97b3ac61de393522b
Oskar Barcz <kontakt@archi-tektur.pl>
2021-07-13 13:07:39 +0200

    Add base test env

--- tests/Infrastructure/Symfony/Controller/UserControllerTest.php
+++ tests/Infrastructure/Symfony/Controller/UserControllerTest.php
@@ -4,56 +4,44 @@ declare(strict_types=1);
 
 namespace Tests\Infrastructure\Symfony\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Tests\TestCase\ApiTestCase;
 
-class UserControllerTest extends WebTestCase
+class UserControllerTest extends ApiTestCase
 {
     public function testLogInWithCorrectCredentials(): void
     {
-        $server = ['CONTENT_TYPE' => 'application/json'];
-
-        $client = static::createClient([], $server);
-
-        $testUserData = [
+        $input = [
             'username' => 'generic1@example.com',
             'password' => 'generic1',
         ];
 
-        $content = json_encode($testUserData);
+        $response = $this->request('POST', 'api/v1/login', $input);
 
+        self::assertSame(200, $response->getStatusCode());
+        self::assertJson($response->getContent());
 
-        $client->request('POST', 'api/v1/login', content: $content);
+        $output = json_decode($response->getContent(), true);
 
-        self::assertSame(200, $client->getResponse()->getStatusCode());
-        self::assertJson($client->getResponse()->getContent());
-
-        $apiReceivedData = json_decode($client->getResponse()->getContent(), true);
-
-        self::assertArrayHasKey('token', $apiReceivedData);
-        self::assertIsString($apiReceivedData['token']);
+        self::assertArrayHasKey('token', $output);
+        self::assertIsString($output['token']);
     }
 
     public function testLogInWithIncorrectCredentials(): void
     {
-        $server = ['CONTENT_TYPE' => 'application/json'];
 
-        $client = static::createClient([], $server);
-
-        $testUserData = [
+        $input = [
             'username' => 'generic1@example.com',
             'password' => 'some-incorrect-password',
         ];
 
-        $content = json_encode($testUserData);
+        $response =$this->request('POST', 'api/v1/login', $input);
 
-        $client->request('POST', 'api/v1/login', content: $content);
+        self::assertSame(401, $response->getStatusCode());
+        self::assertJson($response->getContent());
 
-        self::assertSame(401, $client->getResponse()->getStatusCode());
-        self::assertJson($client->getResponse()->getContent());
+        $output = json_decode($response->getContent(), true);
 
-        $apiReceivedData = json_decode($client->getResponse()->getContent(), true);
-
-        self::assertArrayHasKey('message', $apiReceivedData);
-        self::assertSame('Invalid credentials.', $apiReceivedData['message']);
+        self::assertArrayHasKey('message', $output);
+        self::assertSame('Invalid credentials.', $output['message']);
     }
 }
--- tests/TestCase/ApiTestCase.php
+++ tests/TestCase/ApiTestCase.php
@@ -4,8 +4,42 @@ declare(strict_types=1);
 
 namespace Tests\TestCase;
 
+use App\Domain\User;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiTestCase extends WebTestCase
 {
+    protected KernelBrowser|null $client;
+
+
+    protected function setUp(): void
+    {
+        $this->createApiClient();
+        parent::setUp();
+    }
+    
+    public function auth(User $user): void
+    {
+    }
+
+    public function request(string $method, string $uri, array $content)
+    {
+        if ($this->client === null) {
+            $this->createApiClient();
+        }
+
+        $json = json_encode($content);
+
+        $this->client->request($method, $uri, content: $json);
+
+        return $this->client->getResponse();
+
+    }
+
+    public function createApiClient(): void
+    {
+        $server = ['CONTENT_TYPE' => 'application/json'];
+        $this->client = static::createClient([], $server);
+    }
 }
