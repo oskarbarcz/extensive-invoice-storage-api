28d8860ec42adc45b12bf032dc5c7db77c710637
Oskar Barcz <kontakt@archi-tektur.pl>
2021-07-13 12:46:12 +0200

    Add first TDD functionality

--- tests/Infrastructure/Symfony/Controller/UserControllerTest.php
+++ tests/Infrastructure/Symfony/Controller/UserControllerTest.php
@@ -22,7 +22,7 @@ class UserControllerTest extends WebTestCase
         $content = json_encode($testUserData);
 
 
-        $client->request('POST', 'api/login_check', content: $content);
+        $client->request('POST', 'api/v1/login', content: $content);
 
         self::assertSame(200, $client->getResponse()->getStatusCode());
         self::assertJson($client->getResponse()->getContent());
@@ -31,5 +31,29 @@ class UserControllerTest extends WebTestCase
 
         self::assertArrayHasKey('token', $apiReceivedData);
         self::assertIsString($apiReceivedData['token']);
+    }
+
+    public function testLogInWithIncorrectCredentials(): void
+    {
+        $server = ['CONTENT_TYPE' => 'application/json'];
+
+        $client = static::createClient([], $server);
+
+        $testUserData = [
+            'username' => 'generic1@example.com',
+            'password' => 'some-incorrect-password',
+        ];
+
+        $content = json_encode($testUserData);
+
+        $client->request('POST', 'api/v1/login', content: $content);
+
+        self::assertSame(401, $client->getResponse()->getStatusCode());
+        self::assertJson($client->getResponse()->getContent());
+
+        $apiReceivedData = json_decode($client->getResponse()->getContent(), true);
+
+        self::assertArrayHasKey('message', $apiReceivedData);
+        self::assertSame('Invalid credentials.', $apiReceivedData['message']);
     }
 }
